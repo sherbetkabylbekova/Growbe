d34b2572c31431001d6e85668e4099a71234a80f
Sherbet <sherbetkabylbekova@gmail.com>
2021-06-08 23:11:52 +0600

    js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin: 60,
+        loop: true,
+        center: true,
+        stagePadding: 450,
+
+    });
+});
